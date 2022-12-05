48bcc25eb74bbeda96df9630a88b61231b7667ea
Hugo Brown <titechproduc@gmail.com>
2022-12-05 12:29:01 -0300

    entrega servidor

--- app.js
+++ app.js
@@ -1,16 +1,43 @@
+const { json } = require("express");
 const express = require("express");
 const app = express();
 const path = require("path")
+const PORT = 8080
+const manager = require ("./classContenedor")
 
+//RUTA "/"
+app.get("/", (req,res)=> {
+    res.send ('<h1 style:"red">Bienvenidos</h1>')
+})
 
+//RUTA /PRODUCTS QUE MUESTRE LOS PRODUCTOS TXT
 app.get("/productos", (req,res)=>{
-    res.sendFile (path.join(__dirname + "/productos.txt"));
+    res.sendFile (path.join(__dirname + "/products.txt"));
+})
+//PRODUCTOS RANDOM
+const random = () => Math.floor(Math.random())
+const data = ('/products.txt');
+const state = {
+    randomFav :null,
+    favourites:data
+}
+
+app.get("/productosRandom", async (req,res)=>{
+    let limit = req.query.limit;
+    const products = await manager.getProducts;
+    if (!limit){
+        return res.json(products);
+    }
+    limit = limit<products.length ? limit: products.length;
+    const arr = [];
+    for (let i=0;i< limit; i++){
+        arr.push(products[i]);
+    }
+    return res.json(arr);
 })
 
-app.listen(8080, ()=> {
-    console.log ("servidor running")
+app.listen (8080, ()=> {
+    console.log("server running")
 })
 
-app.get("/", (req,res)=>{
-    res.sendFile (path.join(__dirname + "/classContenedor"));
-})
+
--- classContenedor.js
+++ classContenedor.js
@@ -29,4 +29,61 @@ class ProductManager {
             })
             .then(productsNew => fs.promises.writeFile(this.fileName, JSON.stringify(productsNew)))
     }
+
+    getProductById = async (id) => {
+        const data = await this.getProduct()
+        const productFound = data.find(product => product.id === id)
+        return productFound || console.log(`ERROR: EL PRODUCTO CON EL ID "${id}" NO EXISTE.`);
     }
+
+    getProduct = async () => {
+        const product = fs.promises.readFile(this.fileName, this.format)
+        return product
+            .then(content => JSON.parse(content))
+            .catch(e => {if(e) return []})
+    }
+
+    deleteProduct = async (id) => {
+        const data = await this.getProduct()
+        const toBeDeleted = data.find(product => product.id === id)
+
+        if(toBeDeleted){
+            const index = data.indexOf(toBeDeleted)
+            data.splice(index, 1);
+            await fs.promises.writeFile(this.fileName, JSON.stringify(data))
+            console.log(`\n\nPRODUCTO ELIMINADO: ID "${id}".`);
+        } else {
+            console.log(`\n\nERROR AL ELIMINAR PRODUCTO: EL PRODUCTO CON EL ID "${id}" NO SE ENCUENTRA EN LA LISTA.`);
+        }
+    }
+
+    updateProduct = async (id) => {
+        const data = await this.getProduct()
+        const toBeUpdated = data.find(product => product.id === id)
+
+        toBeUpdated["title"] = "PRODUCTO ACTUALIZADO"
+        toBeUpdated["stock"] = 150
+        
+        fs.writeFileSync(this.fileName, JSON.stringify(data))
+    }
+}
+
+async function run(){
+  const manager = new ProductManager('./products.txt')
+  await manager.addProduct('producto 1', 'descripción 1', 1500, 'N/A', 10, 'abc123')
+  await manager.addProduct('producto 2', 'descripción 2', 2500, 'N/A', 20, 'abc456')
+  await manager.addProduct('producto 3', 'descripción 3', 3500, 'N/A', 30, 'abc789')
+  await manager.addProduct('producto 4', 'descripción 4', 4500, 'N/A', 40, 'def123')
+  await manager.addProduct('producto 5', 'descripción 5', 5500, 'N/A', 50, 'def123')
+ /*console.log("---------------------------------PRODUCTOS AGREGADOS:\n");
+ console.log( await manager.getProduct());
+ console.log("---------------------------------\nPRODUCTO SELECCIONADO:");
+ console.log( await manager.getProductById(1));
+ console.log("---------------------------------")
+ console.log( await manager.getProductById(1));
+ console.log (await manager.deleteProduct(2));
+*/
+}
+
+run();
+module.exports = ProductManager;
